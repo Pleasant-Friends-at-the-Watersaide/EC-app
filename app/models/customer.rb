149dac7482cf6ae3b463f34da11f81bf9bc12db6
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-16 08:08:32 +0000

    [Add]customerモデルにバリデーションとアソシエーションの追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,20 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cart_items, dependent: :destroy
+  has_many :orders    , dependent: :destroy
+  has_many :address   , dependent: :destroy
+
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :kana_last_name
+    validates :kana_first_name
+    validates :postcode
+    validates :address
+    validates :phone_number
+    validates :email, uniqueness: true
+  end
+
 end
