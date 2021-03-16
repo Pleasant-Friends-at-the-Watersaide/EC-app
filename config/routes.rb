d1396274931d52f4a5b25db5449e59e01685de48
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-16 04:04:59 +0000

    [Fix]ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   devise_for :customers
-<<<<<<< HEAD
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
->>>>>>> fe7ab596cd4dad2c26fdf73f3a1d640832ce3ad8
 end
