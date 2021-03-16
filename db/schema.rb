1cbbbe75d6bf0e52f1235bc80f2dae2a7f9fc5bf
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-16 02:56:38 +0000

    [Add]deviseのカラムとビューの追加”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_104437) do
+ActiveRecord::Schema.define(version: 2021_03_16_000105) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_03_15_104437) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "kana_last_name"
+    t.string "kana_first_name"
+    t.string "postcode"
+    t.string "address"
+    t.string "phone_number"
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
