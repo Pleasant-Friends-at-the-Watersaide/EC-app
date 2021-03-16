8ea69a8ba45dea0f45fcbb3331054d9d91a3841e
kenmoi <ken.en001.mb@gmail.com>
2021-03-16 08:25:11 +0000

    [Add]商品・カート・ジャンルのアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to :genre
+  has_many :cart_items
+  has_many :order_details
+  
+  attachment :image
+  
+  validates :introduction,length: {maximum: 200}
 end
