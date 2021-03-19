dfb1ed4387c0f9202e4ecdd9c1580cc0c2b9d078
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-19 12:37:24 +0000

    [Add]商品の新規投稿機能

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,9 +1,17 @@
 class Admins::ItemsController < ApplicationController
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "商品の登録が完了しました。"
+      redirect_to admins_item_path(@item)
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -19,5 +27,10 @@ class Admins::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+    end
 
 end
