c05897f9f41371dd61da3cd9cc3072dfbcd80246
kenmoi <ken.en001.mb@gmail.com>
2021-03-19 12:14:16 +0000

    [Fix] 管理者側商品履歴一覧びstatus表示の修正

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -2,7 +2,7 @@ class Admins::HomesController < ApplicationController
   def top
     path = Rails.application.routes.recognize_path(request.referer)
     if path[:controller] == "admins/customers" && path[:action] == "show"
-     @customer = Customer.find(params[:id])   
+     @customer = Customer.find(params[:id])
      @orders = @customer.order.page(params[:page]).per(10)
     else
      @orders = Order.page(params[:page]).per(10)
