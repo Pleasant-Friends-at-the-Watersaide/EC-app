50c148d2d5126c13aa82b6a401194da453b2d8b1
kenmoi <ken.en001.mb@gmail.com>
2021-03-20 05:16:24 +0000

    [Fix]注文履歴一覧画面の条件分岐修正

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -3,7 +3,7 @@ class Admins::HomesController < ApplicationController
     path = Rails.application.routes.recognize_path(request.referer)
     if path[:controller] == "admins/customers" && path[:action] == "show"
      @customer = Customer.find(params[:id])
-     @orders = @customer.order.page(params[:page]).per(10)
+     @orders = @customer.orders.page(params[:page]).per(10)
     else
      @orders = Order.page(params[:page]).per(10)
     end
