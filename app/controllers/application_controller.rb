568f53e506c02187814e0fe797c2eec449f41d60
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-21 12:25:06 +0000

    [Fix]sign_up,sign_in後の遷移先指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(resourse)
-    root_path
+  
+   def after_sign_up_path_for(resource)
+    new_company_session_path
   end
   
+  def after_inactive_sign_up_path_for(resource)
+    customers_my_page_path
+  end
+
+  def after_sign_in_path_for(resourse)
+    if current_customer
+      root_path
+    elsif current_admin
+      admins_root_path
+    end
+  end
+
   def after_sign_out_path_for(resourse)
     root_path
   end
