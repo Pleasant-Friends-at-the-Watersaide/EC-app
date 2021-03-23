ef66f4cd87edcb9d619b7ecfcc99b427f3694812
Kamezaki-jp <champagne.es335@gmail.com>
2021-03-23 03:31:54 +0000

    [Fix] 管理者の注文履歴詳細のデータ修正

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -10,7 +10,7 @@ class Admins::OrderDetailsController < ApplicationController
 
   private
     def order_detail_params
-    params.require(:order_detail).permit(:production_status, :order_id)
+    params.require(:order_detail).permit(:production_status)
     end
 end
 
